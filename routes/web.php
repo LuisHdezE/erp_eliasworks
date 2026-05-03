fc76ee2c7be9925d7e3a74cdf98556e6b6c97d7a
LuisHdezE <luishe72@yahoo.com>
2026-05-03 09:48:07 -0300

    Add temporary migration route

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,18 @@ use App\Presentation\Livewire\Pages\ProcessPage;
 use App\Presentation\Livewire\Pages\ProjectsPage;
 use App\Presentation\Livewire\Pages\ServicesPage;
 
-Route::get('/', HomePage::class);
+Route::get('/', HomePage::class)->name('home');
+
+// RUTA TEMPORAL PARA MIGRACIONES (Eliminar después de usar)
+Route::get('/migrate', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+        return "Base de datos migrada con éxito: " . \Illuminate\Support\Facades\Artisan::output();
+    } catch (\Exception $e) {
+        return "Error al migrar: " . $e->getMessage();
+    }
+});
+
 Route::get('/catalogo', ApplicationsPage::class);
 Route::get('/app/{slug}', ApplicationDetailPage::class);
 Route::get('/servicios', ServicesPage::class);
